55fd281f9f29a2e9d62bb7d3b8e63323d6f1c487
kent_objectone <Kent.Steinwall@dialog.lk>
2021-06-30 10:26:14 +0530

    initial commit

---
+++ src/main/java/com/example/springAOP/dto/Employee.java
@@ -0,0 +1,15 @@
+package com.example.springAOP.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Employee {
+
+    private String empId;
+    private String name;
+
+    public Employee() {
+    }
+}
